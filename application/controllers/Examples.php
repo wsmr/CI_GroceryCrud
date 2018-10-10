2ded3f0fb24b5f14d8b5bd987c33592d155038fb
wsmr <w.s.m.rathnasekara@gmail.com>
2018-10-10 09:46:57 +0530

    First commit

---
+++ application/controllers/Examples.php
@@ -0,0 +1,132 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Examples extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('url');
+
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function _example_output($output = null)
+	{
+		$this->load->view('example.php',(array)$output);
+	}
+
+	public function offices()
+	{
+		$output = $this->grocery_crud->render();
+
+		$this->_example_output($output);
+	}
+
+	public function index()
+	{
+		$this->_example_output((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
+	}
+
+
+
+
+
+	public function country_management()
+	{
+//		$crud->set_theme('datatables');	
+		$crud = new grocery_CRUD();
+		
+		$crud->set_table('country');
+		$crud->columns('id','name','active');
+		$crud->set_subject('country');
+//		$crud->display_as('field_name','field_label');
+//		$crud->set_relation('id','employees','firstName');
+//		$crud->set_relation('name','employees','lastName');		
+//		$crud->set_relation('active','employees','officeCode');
+		$crud->field_type('active','true_false');
+		$crud->required_fields('name');
+		$output = $crud->render();
+//		var_dump($output);
+//		exit();
+		$this->_example_output($output);
+	}
+
+
+	public function action_item_management()
+	{
+//		$crud->set_theme('datatables');	
+		$crud = new grocery_CRUD();
+		
+		$crud->set_table('action_item');
+		$crud->columns('id','name','active','description','status','owner','due_date','comments','week','month','reviesed_date','qty','target_timeline');
+		$crud->set_subject('action_item');
+		$crud->display_as('target_timeline','Target Time Line');
+		$crud->field_type('active','true_false');
+		$crud->required_fields('name');
+//		$crud->unset_texteditor('name','full_text');
+		$crud->change_field_type('description', 'text');
+		$output = $crud->render();
+ 
+		$this->_example_output($output);
+	}
+
+
+	public function objective_management()
+	{
+//		$crud->set_theme('datatables');	
+		$crud = new grocery_CRUD();
+		
+		$crud->set_table('objectives');
+		$crud->columns('id','name','active','description','country','owner','due_date','comments','week','month','reviesed_date','qty','target_timeline');
+		$crud->set_subject('objectives');
+		$crud->display_as('target_timeline','Target Time Line');		
+		$crud->field_type('active','true_false');
+		$crud->required_fields('name');
+		$crud->change_field_type('description', 'text');		
+		$output = $crud->render();
+		 
+		$this->_example_output($output);
+	}
+
+
+	public function status_management()
+	{
+//		$crud->set_theme('datatables');	
+		$crud = new grocery_CRUD();
+		
+		$crud->set_table('status');
+		$crud->columns('id','name','active');
+		$crud->set_subject('status');
+		$crud->field_type('active','true_false');
+		$crud->required_fields('name');
+		$output = $crud->render();
+
+		$this->_example_output($output);
+	}
+
+
+	public function owner_management()
+	{
+//		$crud->set_theme('datatables');	
+		$crud = new grocery_CRUD();
+		
+		$crud->set_table('owner');
+		$crud->columns('id','name','active','short_count');
+		$crud->set_subject('owner');
+//		$crud->unset_texteditor('name','full_text');
+//		$crud->callback_edit_field( string $name , mixed $callback );
+//		$crud->callback_edit_field('phone', function ($value, $primary_key) {
+//		return '+30 <input type="text" maxlength="50" value="'.$value.'" name="phone" style="width:462px">';});
+
+		$crud->field_type('active','true_false');
+//		$crud->field_type('active','set',array('banana','orange','apple','lemon'));
+		$crud->required_fields('name');
+		$output = $crud->render();
+//		var_dump($output);
+//		exit();		 
+		$this->_example_output($output);
+	}
+
+}
---
+++ application/views/example.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php 
+foreach($css_files as $file): ?>
+	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php endforeach; ?>
+</head>
+<body>
+	<div>
+		<a href='<?php echo site_url('examples/objective_management')?>'>Objectives</a> |
+		<a href='<?php echo site_url('examples/action_item_management')?>'>Action Items</a> |
+		<a href='<?php echo site_url('examples/country_management')?>'>Countries</a> |
+		<a href='<?php echo site_url('examples/owner_management')?>'>Owner</a> |
+		<a href='<?php echo site_url('examples/status_management')?>'>Status</a>
+	</div>
+	<div style='height:20px;'></div>  
+    <div style="padding: 10px">
+		<?php echo $output; ?>
+    </div>
+    <?php foreach($js_files as $file): ?>
+        <script src="<?php echo $file; ?>"></script>
+    <?php endforeach; ?>
+</body>
+</html>
